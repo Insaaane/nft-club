65c33734e7992bf68f0844d26cbd3e2a1e2af6d3
Alexander <dayboval_official@mail.ru>
2024-05-08 00:55:00 +0500

    [add] Всплывающие окна подачи заявки

---
+++ main.js
@@ -0,0 +1,30 @@
+const residencyButtons = document.querySelectorAll('.residency-btn');
+const residencyPopup = document.querySelector('.residency__popup');
+const residencySubmitBtn = document.querySelector('.residency__submit_btn')
+const successResidencyPopup = document.querySelector('.success-residency__popup')
+
+const escPushHandler = (evt) => {
+  if (evt.key === 'Escape') {
+    residencyPopup.hidden = true;
+    successResidencyPopup.hidden = true;
+    document.removeEventListener('keydown', escPushHandler);
+  }
+};
+
+const residencyButtonHandler = (evt) => {
+  evt.preventDefault();
+
+  residencyPopup.hidden = false;
+  document.addEventListener('keydown', escPushHandler);
+};
+
+residencyButtons.forEach((btn) => {
+  btn.addEventListener('click', residencyButtonHandler);
+});
+
+residencySubmitBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  successResidencyPopup.hidden = false;
+  residencyPopup.hidden = true;
+})
